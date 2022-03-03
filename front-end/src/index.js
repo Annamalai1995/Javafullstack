103705571e1551c8c2d7dd844d549c110570db13
Annamalai1995 <s.annamalai1995@gmail.com>
2022-03-03 11:39:37 +0530

    project created

---
+++ front-end/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import  ReactDOM  from 'react-dom'
+//render("what to show","where to show")
+ReactDOM.render(
+    <>
+    <h1>Nothing is more Than  our Happieeness</h1>
+    <p>Annamalai</p>
+    <h2>Happiness Overloaded</h2>
+    </>
+    ,document.getElementById("root")
+    
+    
+
+)
