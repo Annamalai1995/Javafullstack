0d444cda294c260073b071d7c9a46be11c124e51
Annamalai1995 <s.annamalai1995@gmail.com>
2022-03-04 11:31:09 +0530

    firstcomponet

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App, { Hello } from './App'
+import Hey, { Place, Travel } from './ooty'
 //render("what to show","where to show")
 ReactDOM.render(
   <>  
   <App/>
   <Hello/>
-  
+  <Hey/>
+  <Place/>
+  <Travel/>
   </>
     ,document.getElementById("root")    
 )
---
+++ front-end/src/ooty.js
@@ -0,0 +1,15 @@
+import React from "react";
+function Hey()
+{
+    return(<h1>vaipu illa raja</h1>)
+}
+function Place()
+{
+    return(<h1>OOty is the cooleast place</h1>)
+}
+export function Travel()
+{
+    return (<h1> Travel the luxary place</h1>)
+}
+export default Hey;
+export {Place}
